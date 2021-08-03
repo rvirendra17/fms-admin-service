8761cbeaa3eeb3867d6c55cde36366506b97dbbb
= <=>
2021-08-03 16:52:08 +0200

    Updated serviceImpl with error messages

--- src/main/java/com/in/fmc/fmsadminservice/constants/Constants.java
+++ src/main/java/com/in/fmc/fmsadminservice/constants/Constants.java
@@ -4,4 +4,8 @@ public class Constants {
 
 	public static final String FLIGHTS_ADDED_SUCCESSFULLY = "All flights added successfully";
 	public static final String FLIGHTS_ADDED_WITH_WARNING = "Success with warning: few flights already exist. Flights added except - ";
+
+	private Constants() {
+
+	}
 }
