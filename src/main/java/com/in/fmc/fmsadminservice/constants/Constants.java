0aaa570863c125bda9bc9f8ffe79a5ceb2f908d9
= <=>
2021-08-03 16:46:34 +0200

    Added custom exception and messages

---
+++ src/main/java/com/in/fmc/fmsadminservice/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.in.fmc.fmsadminservice.constants;
+
+public class Constants {
+
+	public static final String FLIGHTS_ADDED_SUCCESSFULLY = "All flights added successfully";
+	public static final String FLIGHTS_ADDED_WITH_WARNING = "Success with warning: few flights already exist. Flights added except - ";
+}
---
+++ src/main/java/com/in/fmc/fmsadminservice/constants/ErrorConstants.java
@@ -0,0 +1,8 @@
+package com.in.fmc.fmsadminservice.constants;
+
+public class ErrorConstants {
+
+	public static final String RESPONSE_TYPE_DEFINITION = "https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html";
+	
+	public static final String FLIGHTS_EXIST_EXCEPTION_MSG = "Flights already exist";
+}
