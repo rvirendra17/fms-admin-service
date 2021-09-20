cab9b621a3440be5acbe3b248f34faf55ecc029a
= <=>
2021-09-20 18:23:05 +0200

    FMS-1006 - Added view flights functions for flights

---
+++ src/main/java/com/in/fmc/fmsadminservice/exceptions/FlightsNotFoundException.java
@@ -0,0 +1,15 @@
+package com.in.fmc.fmsadminservice.exceptions;
+
+public class FlightsNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FlightsNotFoundException(String msg) {
+
+		super(msg);
+	}
+
+}
